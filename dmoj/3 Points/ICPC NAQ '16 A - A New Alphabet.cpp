509366b170a99106e78ad13f0db61a9c9fdb6ae7
Alexander Egorov <acegorov7@gmail.com>
2017-07-08 01:49:30 -0400

    added ICPC NAQ 16 A

---
+++ dmoj/3 Points/ICPC NAQ '16 A - A New Alphabet.cpp
@@ -0,0 +1,28 @@
+// ICPC NAQ 2016 A - A New Alphabet
+// dmoj.ca/problem/naq16a
+// 2017-07-07
+// AC in 0.02s
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+    char c;
+    string new_alphabet[26] = { "@", "8", "(", "|)", "3", "#", "6", "[-]", "|", "_|", "|<", "1", "[]\\/[]", "[]\\[]", "0", "|D", "(,)", "|Z", "$", "']['", "|_|", "\\/", "\\/\\/", "}{", "`/", "2" };
+    
+    cin >> noskipws;
+    
+    while (cin >> c)
+    {
+        if (int(c) > 64 && int(c) < 91)
+            cout << new_alphabet[int(c) - 65];
+        else if (int(c) > 96 && int(c) < 123)
+            cout << new_alphabet[int(c) - 97];
+        else
+            cout << c;
+    }
+}
